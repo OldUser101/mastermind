2f96b0a0dd4b4a208df3f61ba6902e84df4ff950
Nathan Gill <nathan.j.gill@outlook.com>
2025-04-29 21:28:24 +0100

    Add copyright message and links.

--- src/App.tsx
+++ src/App.tsx
@@ -133,6 +133,10 @@ export default function App() {
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-check-icon lucide-check"><path d="M20 6 9 17l-5-5"/></svg>
                 </button>
             </div>
+
+            <p>Copyright © 2025, Nathan Gill</p>
+            <a className="underline" href="https://nathanjgill.uk">Website</a>
+            <a className="underline" href="https://github.com/OldUser101/mastermind">GitHub</a>
         </div>
     );
 }
